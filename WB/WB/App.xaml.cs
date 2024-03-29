f250fc4c08589b5aebe1316cbc8f596a2e7ec539
Artem Ibneev <ibneev2015@.mail.ru>
2024-03-29 17:12:33 +0300

    add state handling

--- WB/WB/App.xaml.cs
+++ WB/WB/App.xaml.cs
@@ -14,25 +14,39 @@ namespace WB
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
+        }
+
+        protected override async void OnStart()
+        {
+
+            base.OnStart();
+
             string refreshToken = Preferences.Get(Constants.FIREBASE_TOKEN_KEY, "");
-            Console.WriteLine("TOKENTOKEN" + refreshToken);
-            if (false)
+            // Восстанавливаем предыдущий путь навигации из Preferences
+            string navigationPath = Preferences.Get("NavigationPath", null);
+            if (!string.IsNullOrEmpty(navigationPath))
             {
-                ((App)Application.Current).SwitchToAppShell();
+                await Shell.Current.GoToAsync(navigationPath);
             }
             else
             {
-                MainPage = new NavigationPage(new LoginPage());
+                if (!string.IsNullOrEmpty(refreshToken))
+                {
+                    SwitchToAppShell();
+                }
+                else
+                {
+                    MainPage = new NavigationPage(new LoginPage());
+                }
             }
-            
-        }
-
-        protected override void OnStart()
-        {
         }
 
         protected override void OnSleep()
         {
+            base.OnSleep();
+
+            // Сохраняем текущий путь навигации в Preferences
+            Preferences.Set("NavigationPath", Shell.Current.CurrentState.Location.ToString());
         }
 
         protected override void OnResume()
